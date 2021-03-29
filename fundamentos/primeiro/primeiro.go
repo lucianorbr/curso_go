630f7ef1f690ac16d375b9547fb4b6c915b8651d
Luciano Ribeiro <luciano.ribeiro@outlook.com>
2021-03-29 15:42:52 -0300

    primeiro dia de aulas

---
+++ fundamentos/primeiro/primeiro.go
@@ -0,0 +1,24 @@
+// Progras executeaveis inciam pelo pacote main
+package main
+
+/*
+Os codigos em G sao organizados em pacotes
+e para usa-los é necessario declarar um ou varios imports
+*/
+
+import "fmt" // pacote a ser importado
+
+//A parta de entrada de uma programa Go é a função main
+func main() {
+	fmt.Println("Primeiro ")
+	fmt.Println("Programa")
+
+	/*
+	   Sobre comentário...
+
+	      1)Priorize código legível e faça comentários que agrega valor!
+	      2)Evite comentários óbvios.
+	      3) Durante o curso abuse dos comentários
+	*/
+
+}
