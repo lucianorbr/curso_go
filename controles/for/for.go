8b280c9b758c108944873dc9dbdf7aea08c40962
Luciano Ribeiro <luciano.ribeiro@outlook.com>
2021-04-02 16:46:55 -0300

    Formatos basicos de funcoes

--- controles/for/for.go
+++ controles/for/for.go
@@ -6,28 +6,28 @@ import (
 )
 
 func main() {
-	i := 1
-	for i <= 10000 { // não existe while em Go
-		fmt.Println(i)
-		time.Sleep(time.Second * 5)
-		i++
-	}
-
-	for i := 0; i <= 2000000; i += 1 {
-		fmt.Printf("%d ", i)
-		time.Sleep(time.Second * 5)
-	}
-
-	fmt.Println("\nMisturando... ")
-	for i := 1; i <= 10; i++ {
-		if i%2 == 0 {
-			fmt.Print("Par ")
-			time.Sleep(time.Second * 5)
-		} else {
-			fmt.Print("Impar ")
-			time.Sleep(time.Second * 5)
-		}
-	}
+	//i := 1
+	//for i <= 10000 { // não existe while em Go
+	//	fmt.Println(i)
+	//	time.Sleep(time.Second * 5)
+	//	i++
+	//}
+	//
+	//for i := 0; i <= 2000000; i += 1 {
+	//	fmt.Printf("%d ", i)
+	//	time.Sleep(time.Second * 5)
+	//}
+	//
+	//fmt.Println("\nMisturando... ")
+	//for i := 1; i <= 10; i++ {
+	//	if i%2 == 0 {
+	//		fmt.Print("Par ")
+	//		time.Sleep(time.Second * 5)
+	//	} else {
+	//		fmt.Print("Impar ")
+	//		time.Sleep(time.Second * 5)
+	//	}
+	//}
 
 	//laço infito
 	for {
